89d5c499ca3ba70b481f843d788c57f2d947a2d9
SparkLessExploitCreator <118879929+SparkLessExploitCreator@users.noreply.github.com>
2022-11-25 19:38:01 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -5,8 +5,10 @@ local Gorilla = Window:NewTab("Gorilla")
 local SpeedandJump = Gorilla:NewSection("Speed and Jump")
 SpeedandJump:NewSlider("Speed", "Make u faster", 100, 16, function(s) 
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-    SpeedandJump:NewSlider("JumpPower", "JumpPower", 100, 0, function(s)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+
+    --jumpPOwer
+SpeedandJump:NewSlider("JumpPower", "JumpPower", 100, 0, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
     end)
 end)
 

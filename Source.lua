0b7764543594e9fe712a474094a98d86396b91dc
SparkLessExploitCreator <118879929+SparkLessExploitCreator@users.noreply.github.com>
2022-11-25 19:32:10 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -6,8 +6,10 @@ local SpeedandJump = Gorilla:NewSection("Speed and Jump")
 SpeedandJump:NewToggle("Speed", "Make u faster", function(state)
     if state then
         print("On")
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
     else
         print("Off")
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
     end
 end)
 

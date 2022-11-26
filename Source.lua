8420902314d78c10cadae4897485ef4a7c120773
SparkLessExploitCreator <118879929+SparkLessExploitCreator@users.noreply.github.com>
2022-11-26 09:35:27 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -11,3 +11,17 @@ GorillaSection:NewSlider("JumpPower", "JumpPower", 100, 0, function(v)
     end)
 
 
+
+local Design = Window:NewTab("Design")
+local Keybind = Design:NewSection("Keybind")
+Keybind:NewKeybind("ToggleGui", "ToggleGuiK eybind", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
+
+local Themes = Design:NewSection("Themes")
+for theme, color in pairs(Ocean) do
+    Themes:NewColorPicker(theme, "Change your "..theme, color, function(color3)
+        Library:ChangeColor(theme, color3)
+    end)
+end
+
